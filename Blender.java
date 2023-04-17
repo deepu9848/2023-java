194db5eb47dcac2b542688853a800a02f68930d8
deepu9848 <awasti.deepak2013@gmail.com>
2023-04-17 19:40:53 -0400

    initial commit

---
+++ Blender.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class Blender {
+    /*
+    Blender type
+    Number of blades
+    Blade Material
+    Number of Speed setting
+    On/Off power switch
+    Voltage
+    Capacity
+    Brand Color
+    Dimension
+    Price
+    Warrenty Time
+
+
+
+
+
+     */
+}
